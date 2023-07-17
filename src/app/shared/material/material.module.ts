f05341d66ca8828740ffd71af776af2c007836bd
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-17 15:54:51 -0300

    aula 29

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 
+import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,7 +8,19 @@ import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    LayoutModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    LayoutModule,
+  ],
 })
 export class MaterialModule {}
