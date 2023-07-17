afb94640af55545dacfd43d527d035a218be3e40
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-17 17:03:12 -0300

    aula 32: adding a datepicker

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -14,6 +16,8 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatInputModule,
     LayoutModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   exports: [
     MatButtonModule,
@@ -21,6 +25,8 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatInputModule,
     LayoutModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
 })
 export class MaterialModule {}
