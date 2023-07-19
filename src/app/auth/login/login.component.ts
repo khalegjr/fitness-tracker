18b1058f63cbba850a5008cca94a318140a7e578
Khaled <khalegjr@gmail.com>
2023-07-18 22:13:10 -0300

    tarefa 1: refatorando

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,10 +7,16 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent {
-  loginForm = new FormGroup({
-    email: new FormControl("", [Validators.required, Validators.email]),
-    password: new FormControl("", [Validators.required]),
-  });
+  loginForm!: FormGroup;
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl("", {
+        validators: [Validators.required, Validators.email],
+      }),
+      password: new FormControl("", { validators: [Validators.required] }),
+    });
+  }
 
   onSubmit() {
     console.warn(this.loginForm.value);
