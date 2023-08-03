87f5d65882ef45fb4862ca4e86d817d6d248a254
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-08-03 14:23:46 -0300

    aula 65: setting an active exercise

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -12,7 +12,15 @@ export class TrainingService {
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
   ];
 
+  private runningExercise?: Exercise;
+
   getAvailableExercises() {
     return this.availableExercises.slice();
+  }
+
+  startExercise(selectedId: string) {
+    this.runningExercise = this.availableExercises.find(
+      (ex) => ex.id === selectedId
+    );
   }
 }
